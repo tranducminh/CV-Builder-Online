1c7bbebc587d8c8277b4eddd49fc31298c66caf4
NgTienToan <43052549+NgTienToan@users.noreply.github.com>
2019-10-21 15:53:32 +0700

    cv_section
    cv-section fix boostrap

--- src/src/containers/jobseeker/CVbuild/CVbulid.js
+++ src/src/containers/jobseeker/CVbuild/CVbulid.js
@@ -4,54 +4,54 @@ class CVbulid extends React.Component {
     render() {
         return (
             <div className={`${styles.cv__section}`}>
-                <div className={` ${styles.inner__width}`}>
-                    <h1>Danh sách mẫu CV xin việc hay nhất cho bạn</h1>
+                <div className={` container ${styles.inner__width}`}>
+                    {/* <h1>Danh sách mẫu CV xin việc hay nhất cho bạn</h1> */}
                     <div className={`${styles.boder}`}></div>
-                    <div className={`${styles.cv}`}>
-                        <div className={`${styles.cv__flex}`}>
+                    <div className={`row ${styles.cv}`}>
+                        <div className={`col-lg-3 col-md-5 ${styles.cv__flex}`}>
                             <a className="image">
                                 <img src="https://cdn.timviecnhanh.com/asset/home/img/cv_pattern/cv1_blue_new.png" />
                             </a>
-                            <div className={`${styles.cvTitle}`}>
+                            {/* <div className={`${styles.cvTitle}`}>
                                 <h2>Mẫu CV Chuyên Nghiệp</h2>
-                            </div>
+                            </div> */}
                         </div>
-                        <div className={`${styles.cv__flex}`}>
+                        <div className={` col-lg-3 col-md-5 ${styles.cv__flex}`}>
                             <a className="image">
                                 <img src="https://cdn.timviecnhanh.com/asset/home/img/cv_pattern/cv4_black_new.png" />
                             </a>
                         </div>
-                        <div className={`${styles.cv__flex}`}>
+                        <div className={` col-lg-3 col-md-5 ${styles.cv__flex}`}>
                             <a className="image">
                                 <img src="https://cdn.timviecnhanh.com/asset/home/img/cv_pattern/cv7_green.png" />
                             </a>
                         </div>
-                        <div className={`${styles.cv__flex}`}>
+                        <div className={`col-lg-3 col-md-5 ${styles.cv__flex}`}>
                             <a className="image">
                                 <img src="https://cdn.timviecnhanh.com/asset/home/img/cv_pattern/cv5_black.png" />
                             </a>
                         </div>
-                        <div className={`${styles.cv__flex}`}>
+                        <div className={`col-lg-3 col-md-5 ${styles.cv__flex}`}>
                             <a className="image">
                                 <img src="https://cdn.timviecnhanh.com/asset/home/img/cv_pattern/cv3_pink_new.png" />
                             </a>
                         </div>
-                        <div className={`${styles.cv__flex}`}>
+                        <div className={`col-lg-3 col-md-5 ${styles.cv__flex}`}>
                             <a className="image">
                                 <img src="https://cdn.timviecnhanh.com/asset/home/img/cv_pattern/cv8_violet.png" />
                             </a>
                         </div>
-                        <div className={`${styles.cv__flex}`}>
+                        <div className={`col-lg-3 col-md-5 ${styles.cv__flex}`}>
                             <a className="image">
                                 <img src="https://cdn.timviecnhanh.com/asset/home/img/cv_pattern/cv6_black.png" />
                             </a>
                         </div>
-                        <div className={`${styles.cv__flex}`}>
+                        <div className={`col-lg-3 col-md-5 ${styles.cv__flex}`}>
                             <a className="image">
                                 <img src="https://cdn.timviecnhanh.com/asset/home/img/cv_pattern/cv2_black_new.png" />
                             </a>
                         </div>
-                        <div className={`${styles.cv__flex}`}>
+                        <div className={`col-lg-3 col-md-5 ${styles.cv__flex}`}>
                             <a className="image">
                                 <img src="https://cdn.timviecnhanh.com/asset/home/img/cv_pattern/cv2_black_new.png" />
                             </a>
