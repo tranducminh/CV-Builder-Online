aeb4c7cc807af764ac2270569ce61a96aca37479
Ngọc linh <vuongngoclinh9@gmail.com>
2019-10-22 15:10:56 +0700

    update

--- src/src/containers/App.js
+++ src/src/containers/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Jobseeker from '../containers/jobseeker/JobseekerMain/JobseekerMain'
+import Footer from '../containers/Footer/Footer'
 
 function App() {
   return (
     <div>
       <Jobseeker />
+      <Footer />
     </div>
   );
 }
