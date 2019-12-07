ad952a3afacd859db79befdf7206d076b32f8ac6
Trần Đức Minh <36166484+tranducminh@users.noreply.github.com>
2019-12-07 18:30:34 +0700

    init store

--- src/src/index.js
+++ src/src/index.js
@@ -5,12 +5,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const app = (
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 )
 
 ReactDOM.render(app, document.getElementById('root'));
---
+++ src/src/store/sagas/index.js
@@ -0,0 +1,2 @@
+import { takeEvery } from 'redux-saga/effects';
+import * as actionTypes from '../actions/actionTypes';
